b1b8f0a4855aab9e0487910dc1e39cd7789c85b5
vasiinsopoo <40399772+vasiinsopoo@users.noreply.github.com>
2019-03-20 22:52:49 +0530

    intial remote repo creation
    commited

---
+++ src/com/vmetry/TestLogin.java
@@ -0,0 +1,12 @@
+package com.vmetry;
+
+public class TestLogin {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Test 1"); //added at initial
+
+	}
+
+}
